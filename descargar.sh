12af26649d1255218ab99aea6d4eb2b5369cb0e1
Arian Racca <arianracca@gmail.com>
2023-06-22 17:41:15 -0300

    corrected and debugued

--- descargar.sh
+++ descargar.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-[ $# -eq 0 ] && exit 1 #no se recibieron argumentos
-[ $# -gt 2 ] && exit 2 #se recibio mas de un argumento
+[ $# -eq 0 ] && exit 1 # No se recibieron argumentos
+[ $# -gt 2 ] && exit 2 # Se recibió más de un argumento
 
-archivo=$1
-sumaverif=$2
-key=$((sha256sum $archivo | cut -d " " -f 1))
-echo $key
+archivo="$1"
+sumaverif="$2"
+key="$(sha256sum "$archivo" | cut -d " " -f 1)"
+echo "$key"
 
-if [ key == $((cat sumaverif)) ]; then
-	tar -xv $archivo && exit 0
+if [ "$key" == "$sumaverif" ]; then
+    tar -xvf "$archivo" && exit 0
 else
-	echo "No se pudo descomprimir" && exit 3
-done
+    echo "No se pudo descomprimir" && exit 3
+fi
