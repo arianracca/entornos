e32c9d7a46521f8b31392f6ffb199e4917a54b81
amandatardugno <amandatardugno@gmail.com>
2023-06-22 15:25:00 -0300

    Revert "debuggin iterator"
    This reverts commit 45d747d7e4638a7433c204a2a5652dcf6ca8f5b3.

--- generar.sh
+++ generar.sh
@@ -10,9 +10,8 @@ directorio=/tmp
 
 url=https://source.unsplash.com/random/900%C3%97700/?$tipo
 
-for ((i=1; [ i -gr cantidad - 1 ] ; i++)); do
+for ((i=0; i<cantidad; i++)); do
     nombre_archivo="imagen_$contador.jpg"
     wget -P "$directorio" -O "$nombre_archivo" "$url"
-    echo "$url"
     contador=$((contador+1))
 done
