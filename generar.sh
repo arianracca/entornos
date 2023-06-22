e5f330b1d3bde3318aba44da4250d90dacad2c68
amandatardugno <amandatardugno@gmail.com>
2023-06-22 16:05:40 -0300

    correct iteration

--- generar.sh
+++ generar.sh
@@ -4,14 +4,20 @@
 [ $# -gt 2 ] && exit 2 #se recibio mas de un argumento
 
 cantidad=$1
-tipo=$2a
-directorio=/tmp
-
+tipo=$2
 
 url=https://source.unsplash.com/random/900%C3%97700/?$tipo
+mkdir ./tmp
 
 for ((i=0; i<cantidad; i++)); do
+    contador=$(($i+1))
     nombre_archivo="imagen_$contador.jpg"
-    wget -P "$directorio" -O "$nombre_archivo" "$url"
-    contador=$((contador+1))
+    wget -O "$nombre_archivo" "$url"
+    mv "$nombre_archivo" ./tmp/
+    echo "se creo $nombre_archivo correctamente"
+    sleep 1
 done
+
+tar -cvf comprimido.tar tmp
+
+exit 0
