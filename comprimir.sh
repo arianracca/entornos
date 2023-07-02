38ecb6230033d9da5ea71a2afd2ee506909c3c68
Arian Racca <arianracca@gmail.com>
2023-07-02 20:53:06 -0300

    created list, compressed file and cleaning it before starting

--- comprimir.sh
+++ comprimir.sh
@@ -6,3 +6,9 @@ cd personasimg
 total_personas=0
 total_femeninas=0
 total_masculinas=0
+
+archivo_personas="lista_personas.txt"
+
+archivo_comprimido="imagenes_procesadas.tar.gz"
+
+rm -f "$archivo_personas" "$archivo_comprimido"
