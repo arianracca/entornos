9b74ee48edd1be5754e467e772b07874ddb21256
Arian Racca <arianracca@gmail.com>
2023-07-02 19:15:41 -0300

    updated dataset from dict.csv to dict.txt

--- generar.sh
+++ generar.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 [ $# -eq 0 ] && exit 1 #no se recibieron argumentos
-[ $# -gt 2 ] && exit 2 #se recibio mas de un argumento
+[ $# -gt 1 ] && exit 2 #se recibio mas de un argumento
 
 cantidad=$1
-tipo=$2
 
-url=https://source.unsplash.com/random/900%C3%97700/?$tipo
+url=https://source.unsplash.com/random/900%C3%97700/?person
 mkdir ./tmp
 
 for ((i=0; i<cantidad; i++)); do
-    nombre=$(sort -R ./dict.csv | head -n 1 | cut -d "," -f 1)
-    nombre=${nombre^}
+    nombre=$(sort -R ./dict.txt | head -n 1 | cut -d "," -f 1)
     contador=$(($i+1))
     nombre_archivo="$nombre.jpg"
     wget -O "$nombre_archivo" "$url"
@@ -25,6 +23,3 @@ sha256sum comprimido.tar | cut -d " " -f 1 > sumaverif
 rm -rf ./tmp
 
 exit 0
-
-
-
