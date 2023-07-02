aa6a0c3601460689a3bae39986df917e236054f6
amandatardugno <amandatardugno@gmail.com>
2023-07-02 19:34:11 -0300

    changes in procesar.sh

--- procesar.sh
+++ procesar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd tmp && echo "Funciona" && ls
-for file in ./*.jpg; do
-        nombre=${file^}
-        if [ $nombre = $file ]; then #chequea si la primer letra comienza con m>
-                echo "$nombre es un nombre"
-        else
-                echo "$nombre no es un nombre"
-        fi
+cd tmp
+for file in *.jpg; do
+    nombre=${file%.jpg}  # Eliminamos la extensión .jpg del nombre del archivo
+    primer_letra=${nombre:0:1}  # Obtenemos la primera letra del nombre
+    
+    if [[ $primer_letra =~ [A-Z] ]]; then
+        echo "$nombre es un nombre"
+    else
+        echo "$nombre no es un nombre"
+    fi
 done
-echo "hasta ahora funciona"
-exit 0
