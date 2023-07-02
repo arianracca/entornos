4aa152e8e24d0393f3c8b6f3b13ab6f46c41c5a3
Arian Racca <arianracca@gmail.com>
2023-07-02 19:53:09 -0300

    updated procesar for processing each name on the jpg

--- procesar.sh
+++ procesar.sh
@@ -3,10 +3,20 @@
 cd tmp
 for file in *.jpg; do
     nombre=${file%.jpg}  # Eliminamos la extensión .jpg del nombre del archivo
-    primer_letra=${nombre:0:1}  # Obtenemos la primera letra del nombre
+    palabras=($nombre)  # Dividimos el nombre en palabras
     
-    if [[ $primer_letra =~ [A-Z] ]]; then
-        convert $file -gravity center -resize 512x512+0+0 \ -extent 512x512 $file
+    es_nombre=false
+    for palabra in "${palabras[@]}"; do
+        primer_letra=${palabra:0:1}  # Obtenemos la primera letra de cada palabra
+        
+        if [[ ! $primer_letra =~ [A-Z] ]]; then
+            es_nombre=true
+            break
+        fi
+    done
+    
+    if $es_nombre; then
+        convert $file -gravity center -resize 512x512+0+0 -extent 512x512 $file
     else
         echo "$nombre no es un nombre"
     fi
