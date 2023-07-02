890857aff8e55511b2c0018a3f8ccb186bf621ab
Arian Racca <arianracca@gmail.com>
2023-07-02 20:55:05 -0300

    created iteration for counting names, masc, fem and total

--- comprimir.sh
+++ comprimir.sh
@@ -12,3 +12,21 @@ archivo_personas="lista_personas.txt"
 archivo_comprimido="imagenes_procesadas.tar.gz"
 
 rm -f "$archivo_personas" "$archivo_comprimido"
+
+for file in *.jpg; do
+    nombre=${file%.jpg}  # Eliminamos la extensión .jpg del nombre del archivo
+
+    # Obtener el primer nombre y la última letra
+    primer_nombre=${nombre%% *}  # Obtener el primer nombre
+    ultima_letra=${primer_nombre: -1}  # Obtener la última letra del primer nombre
+
+    # Verificar si es una persona femenina
+    if [[ $ultima_letra == "a" ]]; then
+        echo "$nombre" >> "$archivo_personas"
+        ((total_personas++))
+        ((total_femeninas++))
+    else
+        ((total_personas++))
+        ((total_masculinas++))
+    fi
+done
