d26c4a7beffbc05dc4728b10cbfaed75f6a89aa3
Arian Racca <arianracca@gmail.com>
2023-07-04 06:51:11 -0300

    comment documetation for new script docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 archivo_comprimido="imagenes_procesadas.tar.gz"
+
+# Obtener el ID del último contenedor en ejecución
 id=$(docker ps | cut -d " " -f 1 | tail -n 1)
+
+# Copiar el archivo comprimido desde el contenedor a la ubicación actual
 docker cp $id:/home/"$archivo_comprimido" ./"$archivo_comprimido"
